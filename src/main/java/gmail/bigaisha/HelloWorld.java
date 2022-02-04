41969fe9fce79ac84c3d828f66f3a0df8b109757
Bigaisha <shalabayeva.bigaisha@gmail.com>
2022-02-04 20:35:22 +0600

    Kids

---
+++ src/main/java/gmail/bigaisha/HelloWorld.java
@@ -0,0 +1,11 @@
+package gmail.bigaisha;
+
+public class HelloWorld {
+
+    // модификатор доступа ||static -> возвращаемое значение -> имя -> (аргументы)
+    public static void main(String... args) {
+
+        System.out.println("Hello World!");
+    }
+
+}
