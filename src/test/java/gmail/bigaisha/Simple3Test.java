eb7d80f6cf5c1c8eae157e815bdd3b4c6e1569c8
Bigaisha <shalabayeva.bigaisha@gmail.com>
2022-02-07 21:42:52 +0600

    Simple3Test

--- src/test/java/gmail/bigaisha/Simple3Test.java
+++ src/test/java/gmail/bigaisha/Simple3Test.java
@@ -3,7 +3,7 @@ package gmail.bigaisha;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class SimpleTest {
+public class Simple3Test {
     @Test
     void assertTrueTest() {
         Assertions.assertTrue(3>2);
